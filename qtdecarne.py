21d60f2d4f85bebf57615b2eb871c0fa982aec64
Ana Julia Castelo Branco Fernandes <anajuliacbfernandes@gmail.com>
2023-04-20 09:16:05 -0300

    Create qtdecarne.py

---
+++ qtdecarne.py
@@ -0,0 +1,9 @@
+#entradas
+print ("Quantas pessoas irão ao churras?")
+Numpessoas = int (input ())
+
+#processamento
+Qtdcarne = Numpessoas * 250
+
+#saida
+print ("A quantidade de carne necessária para o seu churras é", Qtdcarne, "gramas.")
